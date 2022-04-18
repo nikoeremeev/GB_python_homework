984fdac8fb142fe6c7c3c1df9fdfb18af5e770b6
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-04-18 12:11:35 +0300

    add task10

---
+++ task10.py
@@ -0,0 +1,5 @@
+# Задача №10: Найти расстояние между двумя точками пространства
+firstPoint= list(map(int, input("Введите координаты первой точки x y z через пробел (например:  2 3 4):").split()))
+secondPoint= list(map(int, input("Введите координаты второй точки x y z через пробел (например:  2 3 4):").split()))
+result = (((secondPoint[0] - firstPoint[0])**2) + ((secondPoint[1] - firstPoint[1])**2) + ((secondPoint[2] - firstPoint[2])**2))**(1/2)
+print(f"Расстояние между двумя точками пространства = {round(result, 2)}")
