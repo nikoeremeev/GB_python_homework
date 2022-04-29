be2de09bf6d53724c006e9fa9d1f168ac7c74e8c
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-04-29 09:26:01 +0300

    add task33

---
+++ task33.py
@@ -0,0 +1,45 @@
+# Задача №33: Задана натуральная степень k. 
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k. 
+# Пример:
+# k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+
+import random
+
+
+def write_file(st):
+    with open('file33.txt', 'w') as data:
+        data.write(st)
+
+
+def rnd():
+    return random.randint(0,101)
+
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1:
+                wr += f'{lst[i]} = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
