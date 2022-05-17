4ea1750b53db787804f0d74e6fdac05c8fddd5c7
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-05-17 06:16:58 +0300

    changed task11

--- task11.py
+++ task11.py
@@ -6,3 +6,8 @@ lst = []
 for i in range(number):
     lst.append((-3)**i)
 print(f"Итоговая последовательность: {lst}")
+
+# улучшение
+
+lst = [(-3)**i for i in range(number)]
+print(f"Итоговая последовательность после применения list comprehension: {lst}")
