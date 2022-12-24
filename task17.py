bf88f66e9bef64fed8c5e8e1b727a6f4bd901143
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-12-24 19:19:12 +0300

    changed task17

--- task17.py
+++ task17.py
@@ -1,21 +1,26 @@
-# Задача №17: Задать список из N элементов, заполненных числами из [-N, N]. 
-# Найти произведение элементов на указанных позициях. 
-# Позиции хранятся в файле file.txt в одной строке одно число
+'''
+Задача №17: 
+Задать список из N элементов, заполненных числами из [-N, N]. 
+Найти произведение элементов на указанных позициях. 
+Позиции хранятся в файле file.txt в одной строке одно число.
+'''
 
 import random
+
+
 def write_file(number):
-    with open('file.txt', 'w') as data:
+    with open('file_task17.txt', 'w') as data:
         for i in range(number):
             data.write(f"{random.randrange(0, 2*number)}\n")
 
 
 def read_file():
-    with open('file.txt', 'r') as data:
-        indexs = list(map(int,data.readlines()))
+    with open('file_task17.txt', 'r') as data:
+        indexs = list(map(int, data.readlines()))
     return indexs
 
-# улучшение 
 
+# улучшение проргаммы
 n = int(input("Введите число N: "))
 lst_number = [i for i in range(-n, n+1)]
 write_file(n)
